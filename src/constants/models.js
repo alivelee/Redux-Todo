d8aeba981d292e1612032abeeb34bcbb1c033ab3
Xu Lei <a871460434@gmail.com>
2016-10-09 09:05:06 +0800

    State

---
+++ src/constants/models.js
@@ -0,0 +1,13 @@
+//Initial State
+
+import Immutable from 'immutable';
+
+export const TodoState = Immutable.fromJS({
+  'todos': [],
+  'todo': {
+    id: '',
+    text: '',
+    updatedAt: '',
+    completed: false,
+  }
+});
