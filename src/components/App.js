7b25419ce504ea7fdd743aa5441495f692604eb3
Xu Lei <a871460434@gmail.com>
2016-10-09 09:14:44 +0800

    Main entrance

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Header from '../containers/HeaderContainer';
+import List from '../containers/ListContainer';
+const App = () => (
+  <div>
+    <Header />
+    <List />
+  </div>
+);
+
+export default App;
